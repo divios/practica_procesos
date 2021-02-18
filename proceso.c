facbd4dadf29d328c952ba0171e73d3b140bcc1f
divios <serberpar@gmail.com>
2021-02-18 09:51:06 +0100

    hito 1

--- proceso.c
+++ proceso.c
@@ -8,14 +8,34 @@
 
 
 int main(int argc, char **argv){
-  if(argc<0){
-    fprintf(stderr, "Argumento no entero o menor que cero\n" );
+
+  int p, n_iti = 1, id = 0;
+
+
+  if(argc != 2 || (p = atoi(argv[1])) == 0) {
+    fprintf(stderr, "Argumentos erroneos, solo se permite 1 y debe ser integer\n");
     exit(-1);
   }
-  if(argc==0){
-    fprintf(stderr, "No hay suficientes argumentos\n", );
+
+  if (p <= 0) {
+      fprintf(stderr, "El argumento debe ser mayor que 0\n");
+      exit(-1);
   }
-  if(argc==1){
-    if()
+
+  char points[p];
+  for(int i = 0; i < p; i++) {
+      points[i] = '*';
   }
+
+  id = fork();
+
+  if(id != 0) exit(-1);
+
+  while (1) {
+      fprintf(stderr, "[%i] Proceso %i [%s] iter %i \n", getpid(), p, points, n_iti);
+      n_iti++;
+      sleep(1);
+  }
+
+
 }
