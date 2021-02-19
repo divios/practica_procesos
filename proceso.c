0384039f1646eed57acfa0684d761040403fa5a9
divios <serberpar@gmail.com>
2021-02-19 15:50:31 +0100

    Fixed fork and check of parameter isNumber

--- proceso.c
+++ proceso.c
@@ -9,14 +9,15 @@
 
 int main(int argc, char **argv){
 
-  int p, n_iti = 1, id = 0;
+  int p, n_iti = 1;
 
 
-  if(argc != 2 || (p = atoi(argv[1])) == 0) {
+  if(argc != 2 || strspn(argv[1], "0123456789") != strlen(argv[1])) {
     fprintf(stderr, "Argumentos erroneos, solo se permite 1 y debe ser integer\n");
     exit(-1);
   }
 
+  p = atoi(argv[1]);
   if (p <= 0) {
       fprintf(stderr, "El argumento debe ser mayor que 0\n");
       exit(-1);
@@ -26,10 +27,6 @@ int main(int argc, char **argv){
   for(int i = 0; i < p; i++) {
       points[i] = '*';
   }
-
-  id = fork();
-
-  if(id != 0) exit(-1);
 
   while (1) {
       fprintf(stderr, "[%i] Proceso %i [%s] iter %i \n", getpid(), p, points, n_iti);
