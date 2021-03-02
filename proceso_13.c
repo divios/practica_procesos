0a3e9b06f709efb42cdd98e1cdcb1d1646ea1ef7
victorgallegazo <75885491+victorgallegazo@users.noreply.github.com>
2021-03-02 13:37:44 +0100

    Add files via upload
    Hace falta programar la señal. Funciona bien hasta que empieza a dar procesos con fallo y no sale del programa, es posible que haya que matar procesos en la señal.

--- proceso_13.c
+++ proceso_13.c
@@ -6,4 +6,39 @@
 #include <string.h>
 #include <signal.h>
 
-int 
+#define NUM_PID 5
+
+pid_t pid[NUM_PID];
+
+void execute(int signal){
+    pause();
+}
+
+int main(){
+    signal(SIGALRM, execute);
+    int i=0;
+    for(i=0; i<NUM_PID;i++){
+        pid[i] = fork();
+        if(pid[i]<0){
+            fprintf(stderr, ("Fallo en el fork "));
+        }
+    }
+
+    while(1){
+         int i=0;
+        for(i=0; i<NUM_PID;i++){
+            pid[i] = fork();
+            if(pid[i]<0){
+                fprintf(stderr, ("Fallo en el fork "));
+                exit(-1);
+            }
+            printf("Mi PID es: %i\n", getpid());
+            /*No se si aqui se deberían matar los procesos o seguir con ellos, hay un momento en el que da error al ejecutar*/
+            /*Igual eso hay que tocarlo en la señal*/
+            alarm(1);
+        }
+        i=0;
+    }
+
+
+}
